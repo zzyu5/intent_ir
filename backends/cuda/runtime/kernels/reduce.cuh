5d84bb03a4dbc3e6cd386544fcd0f900c6151749
zzyu5 <1204122531@qq.com>
2026-01-28 14:30:00 +0800

    cuda: generalize reduce runtime to templated BlockAllreduce

--- backends/cuda/runtime/kernels/reduce.cuh
+++ backends/cuda/runtime/kernels/reduce.cuh
@@ -1,47 +1,50 @@
 #pragma once
 
-#include <math.h>
-
 #include <cub/block/block_reduce.cuh>
 
 namespace intentir_cuda {
 
-template <int BLOCK_THREADS>
-struct BlockAllreduceF32 {
-  using BlockReduce = cub::BlockReduce<float, BLOCK_THREADS>;
+template <typename T, int BLOCK_THREADS>
+struct BlockAllreduce {
+  using BlockReduce = cub::BlockReduce<T, BLOCK_THREADS>;
   typename BlockReduce::TempStorage temp;
-  float out;
+  T out;
 };
 
-template <int BLOCK_THREADS>
-__device__ __forceinline__ float block_allreduce_sum(float v, BlockAllreduceF32<BLOCK_THREADS>* st) {
-  using BlockReduce = cub::BlockReduce<float, BLOCK_THREADS>;
-  const float sum = BlockReduce(st->temp).Reduce(v, cub::Sum());
+template <int BLOCK_THREADS, typename T>
+__device__ __forceinline__ T block_allreduce_sum(T v, BlockAllreduce<T, BLOCK_THREADS>* st) {
+  using BlockReduce = cub::BlockReduce<T, BLOCK_THREADS>;
+  const T sum = BlockReduce(st->temp).Reduce(v, cub::Sum());
   if ((int)threadIdx.x == 0) st->out = sum;
   __syncthreads();
   return st->out;
 }
 
-template <int BLOCK_THREADS>
-__device__ __forceinline__ float block_allreduce_max(float v, BlockAllreduceF32<BLOCK_THREADS>* st) {
-  using BlockReduce = cub::BlockReduce<float, BLOCK_THREADS>;
-  const float mx = BlockReduce(st->temp).Reduce(v, cub::Max());
+template <int BLOCK_THREADS, typename T>
+__device__ __forceinline__ T block_allreduce_max(T v, BlockAllreduce<T, BLOCK_THREADS>* st) {
+  using BlockReduce = cub::BlockReduce<T, BLOCK_THREADS>;
+  const T mx = BlockReduce(st->temp).Reduce(v, cub::Max());
   if ((int)threadIdx.x == 0) st->out = mx;
   __syncthreads();
   return st->out;
 }
 
-template <int BLOCK_THREADS>
-__device__ __forceinline__ float block_allreduce_sum(float v) {
-  __shared__ BlockAllreduceF32<BLOCK_THREADS> st;
+template <int BLOCK_THREADS, typename T>
+__device__ __forceinline__ T block_allreduce_sum(T v) {
+  __shared__ BlockAllreduce<T, BLOCK_THREADS> st;
   return block_allreduce_sum<BLOCK_THREADS>(v, &st);
 }
 
-template <int BLOCK_THREADS>
-__device__ __forceinline__ float block_allreduce_max(float v) {
-  __shared__ BlockAllreduceF32<BLOCK_THREADS> st;
+template <int BLOCK_THREADS, typename T>
+__device__ __forceinline__ T block_allreduce_max(T v) {
+  __shared__ BlockAllreduce<T, BLOCK_THREADS> st;
   return block_allreduce_max<BLOCK_THREADS>(v, &st);
 }
 
-}  // namespace intentir_cuda
+template <int BLOCK_THREADS>
+using BlockAllreduceF32 = BlockAllreduce<float, BLOCK_THREADS>;
 
+template <int BLOCK_THREADS>
+using BlockAllreduceI32 = BlockAllreduce<int, BLOCK_THREADS>;
+
+}  // namespace intentir_cuda
